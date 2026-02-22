df3f3c809660c99f322bc21d86e02552436a5ad7
Alpha72142 <sammurari17@gmail.com>
2026-02-23 02:35:29 +0530

    addeing url

--- roster-backend/src/server.js
+++ roster-backend/src/server.js
@@ -2,10 +2,16 @@ import app from "./app.js";
 import { env } from "./config/env.js";
 import cors from "cors";
 
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin:
+      process.env.CLIENT_URL || "https://roster-backend-7vuh.onrender.com",
   }),
 );
 app.listen(env.port, () => {
